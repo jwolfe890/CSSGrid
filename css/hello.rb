73a0a22c176a0892a60f6f513f6c1fa9f4189b8f
John Wolfe <johnwolfe@Johns-MacBook-Pro.local>
2017-12-10 15:56:19 -0500

    first commit

---
+++ css/hello.rb
@@ -0,0 +1,19 @@
+def solve(arr)
+    
+    arr.each do |num|
+        if num > 0
+          if !(array.includes?(num * -1))
+              return num
+          end 
+          
+        elsif 
+          if !(array.includes?(num * -1))
+            return num
+          end
+        end
+        
+    end 
+    
+end
+
+puts solve([1, -1, 2])
